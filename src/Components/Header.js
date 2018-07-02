912cd2e3c33fd00331887f9e0d5c3264c5ebf356
Tim Baker <tbakerx@gmail.com>
2018-07-02 00:54:21 -0700

    Updating link to blog url

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,7 +30,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
-	    <li><a href="https://timbakerdev.wordpress.com/">Blog</a></li>
+	    <li><a href="https://timbakerdev.blog/">Blog</a></li>
          </ul>
 
       </nav>
