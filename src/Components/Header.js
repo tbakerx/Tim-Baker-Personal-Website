114ffc9223f4e32e5c1f14aa59500bc9a2bfdbd9
TimBaker <unnown_user@localhost.localdomain>
2019-11-24 14:56:38 -0800

    Made a few styling changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,13 +26,11 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	    <li><a className="smoothscroll" href="#resume">Resume</a></li>
+	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
-	    <li><a href="https://timbakerdev.blog/">Blog</a></li>
          </ul>
-
       </nav>
 
       <div className="row banner">
